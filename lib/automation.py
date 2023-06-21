6503cfff067804309507102c6708639215c9b8fa
Nikhil Nishad <nikhilnishad279@gmail.com>
2023-06-21 09:06:46 +0100

    adds browser for local
    Signed-off-by: Nikhil Nishad <nikhilnishad279@gmail.com>

--- lib/automation.py
+++ lib/automation.py
@@ -15,10 +15,13 @@ class Automation:
     def __init__(self, profile_id):
         self.profile_id = profile_id
 
-    async def browser(self, profile_id):
+    async def browser_multilogin(self, profile_id):
         json_response = await HttpClient("http://127.0.0.1:35000/api/v1/profile", logger) \
             .get(f"/start?automation=true&profileId={profile_id}")
         return webdriver.Remote(command_executor=json_response['value'])
+
+    async def browser_local(self, profile_id):
+        return webdriver.Chrome('./chromedriver')
 
     async def generate_instagram_account(self, profile_id):
         CountryId = '15'
@@ -62,5 +65,5 @@ class Automation:
         if message.startswith('This country is currently not available for this service'):
             print('[Error Message]', {'jsonData': jsonData})
 
-        browser = await self.browser(profile_id)
+        browser = await self.browser_local(profile_id)
         await signup(browser, user, profile_id)

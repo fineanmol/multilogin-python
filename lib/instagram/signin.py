aaa8925bab3575b2067d7e01496f2b4fcee54858
fineanmolwork <anmol.agarwal@peopleinteractive.in>
2023-06-28 01:55:32 +0530

    ft: updateBioFixed

--- lib/instagram/signin.py
+++ lib/instagram/signin.py
@@ -1,5 +1,4 @@
 import asyncio
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from lib.instagram.likePosts import like_post
 from lib.instagram.followAccounts import follow_accounts
+from lib.instagram.updateBio import update_profile_bio
 
 
 async def signin(browser):
@@ -26,20 +26,28 @@ async def signin(browser):
     password.send_keys(user.get('password'))
 
     signin_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(.,"Log in")]')))
+    await asyncio.sleep(2)
     signin_button.click()
-    await asyncio.sleep(4)
 
-    time.sleep(5)
+    await asyncio.sleep(3)
     try:
-        save_your_login_info_button = browser.find_element_by_xpath('//button[@type="button"]')
-        if save_your_login_info_button.is_displayed():
-            save_your_login_info_button.click()
-    except Exception as err:
-        print('Error:', err)
-    finally:
+        try:
+            save_your_login_info_button = browser.find_element_by_xpath('//button[@type="button"]')
+            if save_your_login_info_button.is_displayed():
+                save_your_login_info_button.click()
+        except Exception as err:
+            print('Error:', 'save_your_login_info_button NOT FOUND')
+
         print('Login Success, Ready for the next step..\n')
-        time.sleep(2)
+        await asyncio.sleep(3)
         button_notification = browser.find_element_by_xpath('//button[text()="Not Now"]')
         button_notification.click()
-        # await like_post(browser)
-        await follow_accounts(browser)
+       
+    except Exception as err:
+        print('Error:', "button_notification NOT FOUND")
+
+    quote="Embrace the journey, chase your dreams, and let your story inspire others."
+    await update_profile_bio(user.get('email'),user.get('password'), quote, browser)
+    # await like_post(browser)
+    # await follow_accounts(browser)
+  
--- lib/instagram/updateBio.py
+++ lib/instagram/updateBio.py
@@ -1,54 +1,39 @@
-import time
+import asyncio
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def update_profile_bio(username, password, quote):
+async def update_profile_bio(username, password, quote,browser):
     try:
-        # Start the WebDriver and go to Instagram
-        driver = webdriver.Chrome()
-        driver.get("https://www.instagram.com/")
-        time.sleep(2)
-
-        # Log in to Instagram
-        username_field = driver.find_element(By.NAME, "username")
-        username_field.send_keys(username)
-        password_field = driver.find_element(By.NAME, "password")
-        password_field.send_keys(password)
-        login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-        login_button.click()
-        time.sleep(2)
-
-        # Close the "Save Your Login Info" prompt if it appears
-        save_info_button = driver.find_element(By.XPATH, "//button[text()='Not Now']")
-        save_info_button.click()
-        time.sleep(2)
-
-        # Close the "Turn On Notifications" prompt if it appears
-        notifications_button = driver.find_element(By.XPATH, "//button[text()='Not Now']")
-        notifications_button.click()
-        time.sleep(2)
+        await asyncio.sleep(2)
+        wait = WebDriverWait(browser, 30)  # Maximum wait await asyncio of 30 seconds
 
         # Go to the profile page
-        profile_link = driver.find_element(By.XPATH, "//a[@href='/" + username + "/']")
+        profile_link = browser.find_element(By.XPATH, "//a[@href='/" + username + "/']")
         profile_link.click()
-        time.sleep(2)
+        await asyncio.sleep(2)
 
         # Click the "Edit Profile" button
-        edit_profile_button = driver.find_element(By.XPATH, "//button[text()='Edit Profile']")
+        edit_profile_button = browser.find_element_by_xpath('//a[text()="Edit Profile"]')
         edit_profile_button.click()
-        time.sleep(2)
+        await asyncio.sleep(3)
+        try:
+            account_center_button = browser.find_element_by_xpath('//div[@role="button"]')
+            if(len(account_center_button)>0):
+                cross_btn = account_center_button[1].click()
+        except:
+            print('Account Center Button Not Found')
 
         # Clear the existing bio and update it with the random quote
-        bio_textarea = driver.find_element(By.XPATH, "//textarea[@aria-label='Bio']")
+        bio_textarea = browser.find_element(By.XPATH, "//textarea")
         bio_textarea.clear()
         bio_textarea.send_keys(quote)
 
         # Save the updated profile bio
-        submit_button = driver.find_element(By.XPATH, "//button[text()='Submit']")
+        submit_button = browser.find_element(By.XPATH, "//div[text()='Submit']")
+        await asyncio.sleep(2)
         submit_button.click()
-        time.sleep(2)
 
         print("Profile bio updated successfully.")
 
@@ -56,11 +41,5 @@ def update_profile_bio(username, password, quote):
         print(f"An error occurred: {e}")
 
     finally:
-        # Quit the WebDriver
-        driver.quit()
+        browser.quit()
 
-# Example usage
-username = "your_username"
-password = "your_password"
-quote = "This is a random quote. Update your bio with meaningful content."
-update_profile_bio(username, password, quote)
